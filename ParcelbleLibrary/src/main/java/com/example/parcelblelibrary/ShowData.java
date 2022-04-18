57a12cfab5b035db6e7c06cbbfeac0cef938a3c6
ad_narayan <aditya.p@iserveu>
2022-04-18 17:17:48 +0530

    set username

--- ParcelbleLibrary/src/main/java/com/example/parcelblelibrary/ShowData.java
+++ ParcelbleLibrary/src/main/java/com/example/parcelblelibrary/ShowData.java
@@ -1,10 +1,13 @@
 package com.example.parcelblelibrary;
 
 import android.content.Context;
+import android.content.Intent;
 import android.widget.Toast;
 
 public class ShowData {
+    public static String userName;
     public static void Show(Context context, String string){
-        Toast.makeText(context,string,Toast.LENGTH_LONG).show();
+        userName= string;
+
     }
 }
--- app/src/main/java/com/example/isulibrary/MainActivity.java
+++ app/src/main/java/com/example/isulibrary/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ShowData.Show(this,"library");
+//        ShowData.Show(this,"library");
+
     }
 }
