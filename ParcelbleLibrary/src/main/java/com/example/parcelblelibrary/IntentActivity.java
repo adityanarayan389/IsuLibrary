8e7a0e3dce8783ec0756c1d65c4958ff1a4a59f4
ad_narayan <aditya.p@iserveu.in>
2022-04-19 15:10:51 +0530

    finalllll

--- ParcelbleLibrary/src/main/java/com/example/parcelblelibrary/IntentActivity.java
+++ ParcelbleLibrary/src/main/java/com/example/parcelblelibrary/IntentActivity.java
@@ -6,15 +6,16 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 public class IntentActivity extends AppCompatActivity {
-
+   public String name,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intent);
         if(getIntent().hasExtra("data")){
             Pojo data = getIntent().getParcelableExtra("data");
-            String name = data.getUserName();
-            String password = data.getPassWord();
+             name = data.getUserName();
+             password = data.getPassWord();
+
             Toast.makeText(IntentActivity.this,data.getId(),Toast.LENGTH_LONG).show();
         }
 
