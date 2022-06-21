12c5a865ab7781264a6a8e666551b2294c3aef99
ad_narayan <aditya.p@iserveu.in>
2022-06-21 12:50:06 +0530

    sdk

--- ParcelbleLibrary/src/main/java/com/example/parcelblelibrary/IntentActivity.java
+++ ParcelbleLibrary/src/main/java/com/example/parcelblelibrary/IntentActivity.java
@@ -14,14 +14,14 @@ public class IntentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intent);
         if(getIntent().hasExtra("data")){
-            Pojo data = getIntent().getParcelableExtra("data");
-             name = data.getUserName();
-             password = data.getPassWord();
-            Toast.makeText(IntentActivity.this,data.getId(),Toast.LENGTH_LONG).show();
+//            Pojo data = getIntent().getParcelableExtra("data");
+//             name = data.getUserName();
+//             password = data.getPassWord();
+//            Toast.makeText(IntentActivity.this,data.getId(),Toast.LENGTH_LONG).show();
             try {
                 ComponentName cName = new ComponentName("com.example.laststep", "com.example.laststep.MainActivity");
                 Intent intent = new Intent("android.intent.action.MAIN");
-                intent.putExtra("data", data);
+                intent.putExtra("data", getIntent().hasExtra("data"));
                 intent.setComponent(cName);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(intent);
